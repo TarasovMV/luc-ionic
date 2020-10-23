8cbbe49ed80f29b64ca78529495cde159554b42a
Tarasov Maxim <tarasovm2013@gmail.com>
2020-10-23 09:25:47 +0300

    upd

--- src/app/@shared/components/shared-filter/shared-filter.module.ts
+++ src/app/@shared/components/shared-filter/shared-filter.module.ts
@@ -2,10 +2,14 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SharedFilterComponent} from './shared-filter.component';
 import {AngularSvgIconModule} from 'angular-svg-icon';
+import {SharedFilterMenuItemComponent} from './components/shared-filter-menu-item/shared-filter-menu-item.component';
 
 
 @NgModule({
-    declarations: [SharedFilterComponent],
+    declarations: [
+        SharedFilterComponent,
+        SharedFilterMenuItemComponent,
+    ],
     imports: [
         CommonModule,
         AngularSvgIconModule.forRoot(),
