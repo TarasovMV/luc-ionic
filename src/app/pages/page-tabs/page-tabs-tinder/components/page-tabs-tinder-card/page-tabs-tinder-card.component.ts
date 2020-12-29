defdd6316fc94ad36df621d23417d98d98355ea9
TarasovMV <31613333+TarasovMV@users.noreply.github.com>
2020-12-29 20:43:14 +0300

    upd

---
+++ src/app/pages/page-tabs/page-tabs-tinder/components/page-tabs-tinder-card/page-tabs-tinder-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-tabs-tinder-card',
+  templateUrl: './page-tabs-tinder-card.component.html',
+  styleUrls: ['./page-tabs-tinder-card.component.scss'],
+})
+export class PageTabsTinderCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/pages/page-tabs/page-tabs-tinder/page-tabs-tinder.module.ts
+++ src/app/pages/page-tabs/page-tabs-tinder/page-tabs-tinder.module.ts
@@ -2,13 +2,16 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {PageTabsTinderComponent} from './page-tabs-tinder.component';
 import {RouterModule} from '@angular/router';
+import {PageTabsTinderCardComponent} from "./components/page-tabs-tinder-card/page-tabs-tinder-card.component";
+import {SharedModule} from "../../../@shared/shared.module";
 
 
 @NgModule({
-    declarations: [PageTabsTinderComponent],
+    declarations: [PageTabsTinderComponent, PageTabsTinderCardComponent],
     imports: [
         CommonModule,
         RouterModule.forChild([{path: '', component: PageTabsTinderComponent}]),
+        SharedModule,
     ]
 })
 export class PageTabsTinderModule {
